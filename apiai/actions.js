d4af4ac3e0ea2d348d1a4afb62df9245c95b22fc
System <none>
2017-08-16 16:06:37 +0000

    🌑🐬 Updated with Glitch

--- apiai/actions.js
+++ apiai/actions.js
@@ -1,4 +1,13 @@
-module.exports = function(req, res, talks) {
+var moment = require('moment');
+var apiai = require('apiai');
+var apiaiApp = apiai(process.env.APIAI_TOKEN);
+
+module.exports = function(req, res, farconData) {
+  var talks = farconData.talks;
+  var rooms = farconData.rooms;
+  var speakers = farconData.speakers;
+  var uniqueTalks = farconData.uniqueTalks;
+  
   var reply = {};
   var action = req.body.result.action;
   var parameters = req.body.result.parameters;
@@ -10,7 +19,6 @@ module.exports = function(req, res, talks) {
     };
   } else if (action === 'talk.session') {
     var filteredTalks = talks.filter((t) => {return t.Session === String(parameters.number)});
-    console.log(filteredTalks);
     if (filteredTalks.length === 0) {
       reply = {
         'speech': `Hmm, I didn't find any talks during session ${parameters.number}, double check the session number.`,
@@ -18,39 +26,145 @@ module.exports = function(req, res, talks) {
       };
     } else if (filteredTalks.length === 1) {
       reply = {
-        'speech': `In room *${filteredTalks[0].Room}*, ${filteredTalks[0].FullName} is presenting: "_${filteredTalks[0].Title}_"`,
-        'displayText': `In room *${filteredTalks[0].Room}*, ${filteredTalks[0].FullName} is presenting: "_${filteredTalks[0].Title}_"`
+        'speech': `In the "*${filteredTalks[0].Room}*", ${filteredTalks[0].FullName} is presenting: "_${filteredTalks[0].Title}_"`,
+        'displayText': `In the "*${filteredTalks[0].Room}*", ${filteredTalks[0].FullName} is presenting: "_${filteredTalks[0].Title}_"`
       };
     } else if (filteredTalks.length > 1) {
-      var presenters = filteredTalks.reduce((message, presenter, index, presenterArray) => {
-        if (index === 0) {
-          return `${presenter.FullName}`;
-        } else if (index === presenterArray.length-1) {
-          return `${message} and ${presenter.FullName}`;
-        } else {
-          return `${message}, ${presenter.FullName}`;
-        }
-      }, '');
+      var presenters = buildPresenterList(filteredTalks);
       reply = {
-        'speech': `In room *${filteredTalks[0].Room}*, ${presenters} are presenting: "_${filteredTalks[0].Title}_"`,
-        'displayText': `In room *${filteredTalks[0].Room}*, ${presenters} are presenting: "_${filteredTalks[0].Title}_"`
+        'speech': `In the "*${filteredTalks[0].Room}*", ${presenters} are presenting: "_${filteredTalks[0].Title}_"`,
+        'displayText': `In the "*${filteredTalks[0].Room}*", ${presenters} are presenting: "_${filteredTalks[0].Title}_"`
       };
     }
   } else if (action === 'speaker.time') {
     var filteredTalks = talks.filter((t) => {return t.FullName.trim().toLowerCase().includes(parameters.FARCON2017_Speakers.trim().toLowerCase())});
+    if (filteredTalks.length === 1) {
+      var talkId = talks.indexOf(filteredTalks[0]);
+      reply = {
+        'speech': `*${filteredTalks[0].FullName}* is presenting _"${filteredTalks[0].Title}"_ at *${filteredTalks[0].StartTimeMoment.format("h:mm a")}* in the "*${filteredTalks[0].Room}*"\nWould you like to see the abstract?"`
+      };
+    } else if (filteredTalks.length > 1) {
+      cancelContext(req);
+      var talksString = filteredTalks.reduce((message, talk) => {
+        return message + `*${talk.FullName}* is presenting _"${talk.Title}"_  at *${talk.StartTimeMoment.format("h:mm a")}* in the "*${talk.Room}*"\n`
+      }, '');
+      var presenters = buildPresenterList(filteredTalks);
+      reply = {
+        'speech': talksString
+      };
+    } else {
+      cancelContext(req);
+      reply = {
+        'speech': `Sorry, I couldn't find that speaker, try double checking your spelling.`
+      };
+    }
+  } else if (action === 'speaker.bio') {
+    var filteredTalks = talks.filter((t) => {return t.FullName.trim().toLowerCase().includes(parameters.FARCON2017_Speakers.trim().toLowerCase())});
     console.log(filteredTalks);
     if (filteredTalks.length === 1) {
       reply = {
-        'speech': `*${filteredTalks[0].FullName}* is presenting from *${filteredTalks[0].StartTime}* until *${filteredTalks[0].EndTime}* in "*${filteredTalks[0].Room}*"`,
-        'displayText': `*${filteredTalks[0].FullName}* is presenting from *${filteredTalks[0].StartTime}* until *${filteredTalks[0].EndTime}* in "*${filteredTalks[0].Room}*"`
+        'speech': `*${filteredTalks[0].FullName}* is a *${filteredTalks[0].Position}* at *${filteredTalks[0].Organization}*: ${filteredTalks[0].LinkedIn}`,
+        'displayText': `*${filteredTalks[0].FullName}* is a *${filteredTalks[0].Position}* at *${filteredTalks[0].Organization}*`
+      };
+    } else if (filteredTalks.length > 1) {
+      var presenters = buildPresenterList(filteredTalks);
+      reply = {
+        'speech': `I found ${filteredTalks.length} people, try again with the full name: *${presenters}*`,
+        'displayText': `I found ${filteredTalks.length} people, try again with the full name: *${presenters}*`
       };
     } else {
       reply = {
-        'speech': `Uh oh, try again.`,
-        'displayText': `Uh oh, try again.`
+        'speech': `Sorry, I couldn't find that speaker, try double checking your spelling.`,
+        'displayText': `Sorry, I couldn't find that speaker, try double checking your spelling.`
       };
+    }
+  } else if (action === 'speaker.abstract') {
+    var context = req.body.result.contexts[0];
+    console.log(context);
+    var filteredTalks = talks.filter((t) => {return t.FullName.trim().toLowerCase().includes(context.parameters.FARCON2017_Speakers.trim().toLowerCase())});   
+    if (filteredTalks.length === 1) {
+      reply = {
+        'speech': `*Here is the abstract:*\n${filteredTalks[0].Abstract}`
+      };
+    }
+  } else if (action === 'talk.time') {
+    var queryMoment = moment(`2017-08-24 ${parameters.time}`);
+    if (queryMoment < moment('2017-08-24 07:00:00')) {
+      queryMoment = queryMoment.add(12, 'hours');
+    }
+    var filteredTalks = uniqueTalks.filter((t) => {return t.StartTimeMoment <= queryMoment && queryMoment < t.EndTimeMoment});
+    var talksString = filteredTalks.reduce((message, talk) => {
+      return message + `In the "*${talk.Room}*", ${talk.Presenters} ${talk.numOfPresenters > 1 ? 'are' : 'is'} presenting: "_${talk.Title}_"\n`
+    }, '');
+    if (filteredTalks.length > 0) {
+      reply = {
+        'speech': `I found ${filteredTalks.length} ${filteredTalks.length === 1 ? 'talk' : 'talks'}:\n${talksString}`
+      };
+    } else {
+      reply = {
+        'speech': `Sorry, I didn't find any talks at ${queryMoment.format("h:mm a")}.`
+      };
+    }
+  } else if (action === 'room.schedule' || action === 'room.speakers') {
+    var filteredTalks = uniqueTalks.filter((t) => {return t.Room === parameters.FARCON2017_Rooms});
+    var talksString = filteredTalks.reduce((message, talk) => {
+      return message + `From *${talk.StartTimeMoment.format("h:mm a")}* to *${talk.EndTimeMoment.format("h:mm a")}*, in the *${talk.Room}*, ${talk.Presenters} ${talk.numOfPresenters === 1 ? 'is' : 'are'} presenting: "_${talk.Title}_"\n`
+    }, '');
+    if (filteredTalks.length > 0) {
+      reply = {
+        'speech': `I found ${filteredTalks.length} ${filteredTalks.length === 1 ? 'talk' : 'talks'} in the ${parameters.FARCON2017_Rooms}:\n${talksString}`
+      }
+    } else {
+      reply = {
+        'speech': `Sorry, I didn't find any talks in the ${parameters.FARCON2017_Rooms}`
+      };
+    }
+  } else if (action === 'organization.speakers') {
+    var filteredTalks = speakers.filter((t) => {return t.Organization === parameters.FARCON2017_SpeakerOrgs});
+    var speakerString = filteredTalks.reduce((message, talk) => {
+      return message + `*${filteredTalks[0].FullName}* is a *${filteredTalks[0].Position}*: ${filteredTalks[0].LinkedIn}"\n`
+    }, '');
+    reply = {
+      'speech': `I found ${filteredTalks.length} ${filteredTalks.length === 1 ? 'speaker' : 'speakers'} from ${parameters.FARCON2017_SpeakerOrgs}:\n${speakerString}`
+    };
+  } else if (action === 'test.followup') {
+    console.log('followup action');
+    console.log(req.body);
+    // cancelContext(req);
+    
+  } else if (action === 'test.followup.yes') {
+    console.log('followup action - yes!');
+    console.log(req.body);
+    console.log(req.body.result.contexts);
+    reply = {
+      'speech': 'Here is your followup!'
     }
   }
   
   res.status(200).json(reply);
 };
+
+function buildPresenterList(filteredTalks) {
+  return filteredTalks.reduce((message, presenter, index, presenterArray) => {
+    if (index === 0) {
+      return `${presenter.FullName}`;
+    } else if (index === presenterArray.length-1) {
+      return `${message} and ${presenter.FullName}`;
+    } else {
+      return `${message}, ${presenter.FullName}`;
+    }
+  }, '');
+}
+
+function cancelContext(req) {
+  var options = {
+    sessionId: req.body.sessionId
+  };
+
+  var request = apiaiApp.deleteContextsRequest(options);
+
+  request.on('error', function(error) {
+      console.log(error);
+  });
+  request.end();
+}
--- server.js
+++ server.js
@@ -5,6 +5,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var moment = require('moment');
+var db = require('monk')(process.env.MONGODB_URI);
+var messages = db.get('messages');
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -12,11 +16,28 @@ var csv = require("fast-csv");
 var apiaiHandler = require('./apiai/actions');
 
 var talks = [];
-csv.fromPath("./data/FARCON2017Schedule.csv", {headers:true})
-   .on("data", function(data){
-     talks.push(data);
+var rooms = [];
+var uniqueTalks = [];
+var speakers = [];
+var organizations = [];
+var farconData = {};
+
+csv.fromPath("./data/FARCON2017ScheduleOrdered.csv", {headers:true})
+   .on("data", function(t){
+     t.StartTimeMoment = moment(`2017-08-24 ${t.StartTime}`);
+     t.EndTimeMoment = moment(`2017-08-24 ${t.EndTime}`);
+     talks.push(t);
+    
    })
    .on("end", function(){
+     rooms = buildRooms(talks);
+     uniqueTalks = buildUniqueTalks(talks);
+     speakers = buildSpeakers(talks);
+     farconData.talks = talks;
+     farconData.rooms = rooms;
+     farconData.uniqueTalks = uniqueTalks;
+     farconData.speakers = speakers;
+     //console.log(uniqueTalks);
      console.log("done");
    });
 
@@ -52,7 +73,8 @@ var dreams = [
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
 app.post("/apiai", function (request, response) {
   console.log(request.body);
-  apiaiHandler(request, response, talks);
+  messages.insert(request.body);
+  apiaiHandler(request, response, farconData);
 });
 
 
@@ -60,3 +82,106 @@ app.post("/apiai", function (request, response) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+function buildPresenterList(filteredTalks) {
+  return filteredTalks.reduce((message, presenter, index, presenterArray) => {
+    if (index === 0) {
+      return `${presenter.FullName}`;
+    } else if (index === presenterArray.length-1) {
+      return `${message} and ${presenter.FullName}`;
+    } else {
+      return `${message}, ${presenter.FullName}`;
+    }
+  }, '');
+}
+
+function buildUniqueTalks(talks) {
+  var rooms = [];
+  var timeList = [];
+  
+  // Find unique list of rooms
+  var roomList = talks.map((t) => {
+    return t.Room;
+  }).filter((value, index, self) => { 
+    return self.indexOf(value) === index;
+  });
+  
+  // Find unique list of times
+  var timeList = talks.map((t) => {
+    return t.StartTime;
+  }).filter((value, index, self) => { 
+    return self.indexOf(value) === index;
+  });
+  
+  var uniqueTalks = roomList.reduce((list, r) => {
+    var talksInRoom = talks.filter((t) => {
+      return t.Room === r;
+    });
+    timeList.map((time) => {
+      var uniqueTalkList = talksInRoom.filter((t) => {
+        return t.StartTime === time;
+      });
+      if (uniqueTalkList.length > 0) {
+        uniqueTalkList[0].numOfPresenters = uniqueTalkList.length;
+        uniqueTalkList[0].Presenters = buildPresenterList(uniqueTalkList);
+        list.push(uniqueTalkList[0])
+      }
+      return;
+    })
+    return list;
+  }, []);
+  return uniqueTalks;
+}
+
+function buildRooms(talks) {
+  var rooms = [];
+  var timeList = [];
+  
+  // Find unique list of rooms
+  var roomList = talks.map((t) => {
+    return t.Room;
+  }).filter((value, index, self) => { 
+    return self.indexOf(value) === index;
+  });
+  
+  // Find unique list of times
+  var timeList = talks.map((t) => {
+    return t.StartTime;
+  }).filter((value, index, self) => { 
+    return self.indexOf(value) === index;
+  });
+  
+  var rooms = roomList.map((r) => {
+    var talksInRoom = talks.filter((t) => {
+      return t.Room === r;
+    });
+    timeList.map((time) => {
+      talksInRoom.filter((t) => {
+        return t.StartTime === time;
+      })
+    })
+    talksInRoom[0].numOfPresenters = talksInRoom.length;
+    talksInRoom[0].Presenters = buildPresenterList(talksInRoom);
+    return talksInRoom[0];
+  });
+  return rooms;
+}
+
+function buildSpeakers(talks) {
+  var speakers = [];
+  
+  // Find unique list of speakers
+  var speakerList = talks.map((t) => {
+    return t.FullName;
+  }).filter((value, index, self) => { 
+    return self.indexOf(value) === index;
+  });
+  
+  var speakers = speakerList.map((s) => {
+    var talksBySpeaker = talks.filter((t) => {
+      return t.FullName === s;
+    });
+    return talksBySpeaker[0];
+  });
+  return speakers;
+}
